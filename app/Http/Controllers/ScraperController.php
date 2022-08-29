71237e75ad76413cd10c5b11b3ddfb5487f5202f
Kaleffdev <markskalijevs@gmail.com>
2022-08-29 03:36:33 +0300

    corrected migration

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -13,7 +13,7 @@ class ScraperController extends Controller
     protected $infoArray = [];
 
     private $queryArray = [];
-    public function index()
+    public function store()
     {
         $client = new Client();
         $crawler = $client->request('GET', 'https://news.ycombinator.com/');
@@ -34,42 +34,42 @@ class ScraperController extends Controller
             $crawler->filter('.subtext')->each(function ($node) {
                 $allText = $node->text();
                 // If the listing is an add (without points) the points are set to 0
-                if(strpos($allText, 'point') !== false) {  
+                if (strpos($allText, 'point') !== false) {
                     $this->infoArray['score'][] = (int) substr($allText, 0, strpos($allText, 'point'));
-                }
-                else {
+                } else {
                     $this->infoArray['score'][] = 0;
                 }
-            }); 
+            });
             // Scrape timestamp
             $crawler->filter('.age')->each(function ($node) {
                 $this->infoArray['age'][] = str_replace('T', ' ', $node->attr('title'));
-            }); 
+            });
             // Scrape unique id and convert to integer
             $crawler->filter('.athing')->each(function ($node) {
                 $this->infoArray['id'][] = (int) $node->attr('id');
-            }); 
+            });
             try {
                 // Contrinue scraping to the next page
                 $link = $crawler->filter('.morelink')->link();
-            } catch(Exception $ex) {
+            } catch (Exception $ex) {
                 // Stop scraping on the last page
                 break;
             }
             // Sleep to prevent being timed out of the website with random intervals
             sleep(rand(3, 15));
+            // Transition to the next page
             $this->counter++;
             $crawler = $client->click($link);
-        } while($link);
+        } while ($link);
         // Re
-        for($i=0; $i < count($this->infoArray['id']); $i++) {
+        for ($i = 0; $i < count($this->infoArray['id']); $i++) {
             $this->queryArray[] = [
                 'id' => $this->infoArray['id'][$i],
                 'rank' => $this->infoArray['rank'][$i],
                 'title' => $this->infoArray['title'][$i],
                 'link' => $this->infoArray['link'][$i],
                 'score' => $this->infoArray['score'][$i],
-                'timestamp' => $this->infoArray['age'][$i]
+                'created_at' => $this->infoArray['age'][$i]
             ];
         }
         dd($this->queryArray);
--- database/migrations/2022_08_29_003536_create_listings_table.php
+++ database/migrations/2022_08_29_003536_create_listings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNewsListingsTable extends Migration
+class CreateListingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateNewsListingsTable extends Migration
      */
     public function up()
     {
-        Schema::create('news_listings', function (Blueprint $table) {
+        Schema::create('listings', function (Blueprint $table) {
             $table->id();
             $table->integer('rank');
             $table->string('title');
@@ -30,6 +30,6 @@ class CreateNewsListingsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('news_listings');
+        Schema::dropIfExists('listings');
     }
 }
